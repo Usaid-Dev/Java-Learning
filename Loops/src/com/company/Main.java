ae5b6e39cea2317a8d7c9293f146a69e7c27da1e
Usaid-Dev <usaidasif12@gmail.com>
2021-01-24 22:46:54 +0500

    - Learn For loop  - learn While Loop

---
+++ Loops/src/com/company/Main.java
@@ -0,0 +1,36 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // For Loop
+        String[] friends = {
+                "Usaid", "Abrar", "Asaad", "Arsalan", "Hamza"
+        };
+
+        for (int loopVar = 0; loopVar < friends.length; loopVar++) {
+            System.out.println(friends[loopVar]);
+        }
+
+        //for table
+        int table = 8;
+        int tableLength = 15;
+
+        for (int tableNumber = 1; tableNumber <= tableLength; tableNumber++) {
+            //2 X 1 = 2
+            System.out.println(table + " X " + tableNumber + " = " + (table * tableNumber));
+        }
+
+
+        // while loop
+        String[] names = {"Usaid", "Abrar", "ASA"};
+        int arrayIndex = 0;
+
+        while (arrayIndex < names.length) {
+            System.out.println(names[arrayIndex++]);
+        }
+
+
+    }
+}
---
+++ Loops/src/com/company/WhileLoop.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class WhileLoop {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean shouldRun = true;
+        while (shouldRun){
+            System.out.print("Enter Your Command : ");
+            String command = scanner.nextLine();
+            if (command.equals("Abai jaa") || command.equals("Allah Hafiz")){
+                shouldRun = false;
+            }
+        }
+
+        String[] names = {"Usaid","Abrar","ASA"};
+        int arrayIndex = 0;
+
+        while (arrayIndex < names.length){
+            System.out.println(names[arrayIndex++]);
+        }
+
+    }
+
+}
