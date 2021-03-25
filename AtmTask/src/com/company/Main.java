97e2118190ace8fd95581f1508943edb8a83d8df
Usaid-Dev <usaidasif12@gmail.com>
2021-03-25 16:01:32 +0500

    -Atm task done..!

--- AtmTask/src/com/company/Main.java
+++ AtmTask/src/com/company/Main.java
@@ -1,62 +1,108 @@
 package com.company;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+    private static Scanner in;
+    private static float balance = 0; // For everyone they will get 0
+    private static int anotherTransaction;
+    private static ArrayList<String> logs;
 
-    static Scanner scanner;
-    static int marks;
-
-    public static void main(String[] args) {
-        scanner = new Scanner(System.in);
-        pin();
-    }
-
-    public static void pin(){
-
+    private static void pin() {
         System.out.println("Enter Your Pin Code and please input number not alphabet");
-        int input = scanner.nextInt();
+        int input = in.nextInt();
         if (input == 1234) {
-            menu();
-        }
-        else{
+            transaction();
+        } else {
             System.out.println("Invalid.. Please Try Again");
             pin();
         }
-
     }
 
-    public static void menu() {
-        menuItem(1, "add");
-        menuItem(2, "subtract");
-        menuItem(3, "view");
-        menuItem(0, "exit");
-        int selection = scanner.nextInt();
-        if (selection == 0 || selection == 3) {
-            System.out.println("Final Marks are : " + marks);
-            if (selection == 0) {
-                return;
-            } else {
-                menu();
-                return;
-            }
-        }
-        System.out.print("Enter marks : ");
-        int marksInput = scanner.nextInt();
-        switch (selection) {
+    private static void greetings() {
+        System.out.println("Welcome to Atm you can withdraw, deposit, and balance you have $0");
+    }
+
+    public static void main(String[] args) {
+        in = new Scanner(System.in);
+        logs = new ArrayList<>();
+        pin();
+        greetings();
+        transaction();
+    }
+
+
+    public static void transaction() {
+        int choice;
+        System.out.println("1. Withdraw ");
+        System.out.println("2. Deposit");
+        System.out.println("3. Balance");
+        System.out.println("4. View Transactions");
+        System.out.println("---------------------" +
+                "Please select an option: " +
+                "---------------------");
+
+        choice = in.nextInt();
+
+        switch (choice) {
             case 1:
-                marks += marksInput;
+                float amount;
+                System.out.println("Please enter an amount you would like to withdraw.");
+                amount = in.nextFloat();
+                if (amount > balance || amount == 0) {
+                    System.out.println("You have a insufficient with your funds\n\n");
+                    anotherTransaction(); // If they want another transaction
+                } else {
+                    // They have some money
+                    // update balance
+                    balance = balance - amount;
+                    String log = "You have withdrawn " + amount + " and your new balance is now. " + balance;
+                    logs.add(log);
+                    System.out.println(log);
+                    anotherTransaction();
+                }
                 break;
             case 2:
-                marks -= marksInput;
+                // This is to deposit
+                float deposit;
+                System.out.println("Please enter the amount you would like to put in: ");
+                deposit = in.nextFloat();
+                // update balance
+                balance = deposit + balance;
+                String log = "You have deposited " + deposit + " new balance is. " + balance;
+                logs.add(log);
+                System.out.println(log);
+                anotherTransaction();
                 break;
-            default:
-                System.out.println("Invalid option selected");
+
+            case 3:
+                // to balance
+                System.out.println("Your balance is " + balance + "\n");
+                anotherTransaction();
+                break;
+
+            case 4:
+                // to view transactions
+                for (String transaction : logs) {
+                    System.out.println(transaction);
+                }
+                anotherTransaction();
+                break;
+
         }
-        menu();
     }
 
-    public static void menuItem(int optionSelector, String name) {
-        System.out.println(optionSelector + " to " + name + " marks");
+    private static void anotherTransaction() {
+        System.out.println("Do you want another transaction? \n\nPress 1 for anotherTransaction \n2 To exit.");
+        anotherTransaction = in.nextInt();
+        if (anotherTransaction == 1) {
+            transaction(); // Call method
+        } else if (anotherTransaction == 2) {
+            System.out.println("Thank you for coming to Atm");
+        } else {
+            System.out.println("Invalid choice \n\n");
+            anotherTransaction();
+        }
     }
 }
